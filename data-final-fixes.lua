9d36255eef1052636662d5b01f735dd05f80ce24
robot256 <robot256@GMAIL.COM>
2025-12-19 11:14:48 -0500

    Summarize logs of tile collision mask changes
    Fixes #52

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -98,23 +98,33 @@ end
 
 if data.raw.resource["offshore-oil"] then
   -- Add new "water_resource" collision layer to all the tiles that have "ground_tile"
+  local count = 0
   for name, tile in pairs(data.raw.tile) do
     local collision_mask = tile.collision_mask
     if collision_mask.layers["ground_tile"] then
-      log("Adding collision layer 'water_resource' to tile '"..name.."'")
+      --log("Adding collision layer 'water_resource' to tile '"..name.."'")
       collision_mask.layers["water_resource"] = true
+      count = count + 1
     end
+  end
+  if count > 0 then
+    log("Added collision layer 'water_resource' to "..tostring(count).." ground tiles.")
   end
 
   -- Add new "water_resource" collision layer to all non-deep water tiles if "Offshore oil on Deep Water only" is enabled
+  count = 0
   if settings.startup["no_shallow_oil"].value then
     for _, name in pairs({"water", "water-green", "water-shallow", "water-mud"}) do
       if data.raw.tile[name] then
         local collision_mask = data.raw.tile[name].collision_mask
-        log("Adding collision layer 'water_resource' to tile '"..name.."'")
+        --log("Adding collision layer 'water_resource' to tile '"..name.."'")
         collision_mask.layers["water_resource"] = true
+        count = count + 1
       end
     end
+  end
+  if count > 0 then
+    log("Added collision layer 'water_resource' to "..tostring(count).." shallow water tiles.")
   end
 
   -- Make sure the oil rig can mine deep oil:
