1f558de1088cff71e46af6cc4bf54632c33dcc6f
robot256 <robot256@GMAIL.COM>
2026-06-23 23:01:37 -0400

    Allow building Floating Electric Pole in Electromagnetics Plant.

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -4,26 +4,31 @@ local collision_mask_util = require("__core__/lualib/collision-mask-util")
 data.raw.tile["landfill"].check_collision_with_entities = true
 
 -- Apply Space Age power pole tile settings
-if settings.startup["floating_pole_enabled"].value and mods["space-age"] and 
-  ((not settings.startup["floating_pole_fulgora"].value) or (not settings.startup["floating_pole_aquilo"].value)) then
-  
-  data:extend{
-    {
-      type = "collision-layer",
-      name = "offshore_pole",
-    },
-  }
-  if not settings.startup["floating_pole_fulgora"].value then
-    data.raw.tile["oil-ocean-shallow"].collision_mask.layers.offshore_pole = true
-    data.raw.tile["oil-ocean-deep"].collision_mask.layers.offshore_pole = true
+if settings.startup["floating_pole_enabled"].value and mods["space-age"] then
+  if ((not settings.startup["floating_pole_fulgora"].value) or (not settings.startup["floating_pole_aquilo"].value)) then
+    data:extend{
+      {
+        type = "collision-layer",
+        name = "offshore_pole",
+      },
+    }
+    if not settings.startup["floating_pole_fulgora"].value then
+      data.raw.tile["oil-ocean-shallow"].collision_mask.layers.offshore_pole = true
+      data.raw.tile["oil-ocean-deep"].collision_mask.layers.offshore_pole = true
+    end
+    
+    if not settings.startup["floating_pole_aquilo"].value then
+      data.raw.tile["ammoniacal-ocean"].collision_mask.layers.offshore_pole = true
+      data.raw.tile["ammoniacal-ocean-2"].collision_mask.layers.offshore_pole = true
+    end
+    
+    data.raw["electric-pole"]["floating-electric-pole"].tile_buildability_rules[1].colliding_tiles = {layers={offshore_pole=true}}
   end
-  
-  if not settings.startup["floating_pole_aquilo"].value then
-    data.raw.tile["ammoniacal-ocean"].collision_mask.layers.offshore_pole = true
-    data.raw.tile["ammoniacal-ocean-2"].collision_mask.layers.offshore_pole = true
-  end
-  
-  data.raw["electric-pole"]["floating-electric-pole"].tile_buildability_rules[1].colliding_tiles = {layers={offshore_pole=true}}
+end
+
+-- Make floating poles in electromagnetics plant
+if data.raw['recipe-category']['electromagnetics'] and data.raw.recipe["floating-electric-pole"] then
+  data.raw.recipe["floating-electric-pole"].categories = {'crafting', 'electromagnetics'}
 end
 
 -- Change inserters to not catch fish when waiting for ships
