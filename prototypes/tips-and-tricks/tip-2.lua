c0594d5dc597d92edd6480589492663ddd60f999
Jurgen <jurgen.baas@outlook.com>
2026-07-08 20:30:22 +0200

    Fixed pumps not working by powering them

--- prototypes/tips-and-tricks/tip-2.lua
+++ prototypes/tips-and-tricks/tip-2.lua
@@ -12,7 +12,7 @@ player.force.research_all_technologies()
 game.simulation.camera_player = player
 game.simulation.camera_position = {0, 0.5}
 game.simulation.camera_player_cursor_position = player.position
--- game.simulation.camera_zoom = 0.5
+game.simulation.camera_zoom = 1.25
 
 ---@type LuaSurface
 local surface = game.surfaces[1]
@@ -29,7 +29,7 @@ end
 
 surface.set_tiles(water_tiles, true)
 
-local bp = "0eNqtmN2uoyAQx1+l4do2BcWP3uztvsOmMVSpJUfBILbbnPjui3p62rOrBybZpEmrZX78HWcGmHd0qnveaiENOrwjUSjZocOvd9SJSrJ6vCdZw9EB3Zjh+sbuaAiQkCX/jQ54CBYGKlHnhsk3rl+GkuEYIC6NMILPE0wX91z2zcmOPODgYd8ZzUR1MdvPGQPUqs5aKjlOY2lbkgXobr+tAlQKzYv5z2gU9BeYwMCJNzj8BBdMVyrvLqLNuayE5EvgeJfgNKFPPJfsVPO8VpXojCi6/HYR9rpRVyErdDizuuMBUlrYedmM2u+IBQjDm9mHonzxu+yLmjO9Pfe8njw/D7OjZC7k1VKUvs9mzyvr9s6w4s2yh+NgP/8+ZwRzIPV2IIWBQ29wDANjb3ACAmP/IE1hYP8gzYKFtFwgxrs0/j/xuSAC72GP5x9CGFY2sH8MYVjdwP5BhEMQ2T+IMCxT/YMIwzIV8PpgmQp4e89MbUW7VI/xbpYZ7eiwBABmpL+yzKVs/70ysncAXPaglPF/MEIcuhweJ6HDnjjsQdHvH0qEftG1NWpbadXLcgH6ovALF5MlcOwNjtbAi4JBq5R/spLU8YKo4wW5Ij/+3j4ErSKAfRx26EoculyBnzrsQeuB/3IQRg5dmUMXdSW0o9KEoOoOWEHDl+qutFll/Y2a9rrzb/RT3TalKDd31W/O9pCyafgPtDTX60KgNKtsotpd1MKcj/pEV7yRgbzhX6Ai14KAH3UpWVYWYV9AugIgvoBsBRD6AsaYWyRE3gS8QqDeBLJCiL0J4QoBVLoB++QItJ0BnC8iWFT7Vy8KqvaAMxwF7X8Ap04KOjFAzsmgBQLQwaCgLdNqz+Vo62px4WVff7R2alWoRhlxna7Dl/9HlLVUuvzoMn1fjwN0Y8LkhZLlpGQ2Mvd2lMyb1owy7Vwt0zz/uM3sBmo4Ts2Msd6r4m20l7Pcx6xq7HjY5zlNrY9w6mPNN62vz1qN3bBsBIxNFAt9NskCdOW6mzTTmGQ0IlGaJWkU2oT+A+y9W+o="
+local bp = "0eNqlmF2PoyAUhv9Kw7VtBMWP3uzt/odJY6hSh4yCQZ1ud9L/vqAzbWdXCyfbTMZq4eFFzsvH+UDHZuSdFnJA+w8kSiV7tH/5QL2oJWvsM8lajvbozAauz+yCrgESsuK/0B5fg4WCSjTFwOQb1w9FyfUQIC4HMQg+NzDdXAo5tkdTco+Dr/r9oJmoX4ftrcUAdao3NZW0zRjaluQBupirUYAqoXk5/xhbQX+BCQyceoOjG7hkulZF/yq6gstaSL4ETnYZyTChdz6X7NjwolG16AdR9sX5VZj7Vr0LWaP9iTU9D5DSwjTMZla4s4BSNUpbsLZP8ji3nywL0zxMcRjHOcFZasoZSBig4/SfTcOFxMDb+f2L6mHM5Fg2nOntaeQNuhczpWQh5LsRoPRlrna/M0PWD6x8s+TD1fz9+45i2Mun3i+fwsCRNziBgbE3OAWBsX+AZzCwf4DnwYKlF4jJLs/COJlC7j9je0EFDmH9848hDJtzsH8QYdikg/2jCEcgsn8UYZhV/aMIw6wKGD6YVQGjd7dqJ7qlyRzvZpnxjl6XAEBL+ivLXcrC58pI6AC46t8t0/JKjO2WN0awFuW2U80THl3hgYzi/6JI5OinYwRJ7KhPHPVBQe8fmiT5pms7qG2t1SirBeiDwm9cTJbAqTc4XgMvCgZZwd/8xOUE+nyAIpcREkd90NoB2FQSh67UoSsCGjR9btDIZYTMoQdkBP/lKkocunKHLtccjx0zYQQKbMAKHz1EttLDKutv1LQZn7+jn+q8qUS1uahxczInsE3Lf6Cl7fnj3kppVhvjm23eQpvkeZjEIDsAdlKxyw/4a55LV5RFvoBsBRD7AvIVAPUF2JhbJCTeBLxCSL0JZIWQeROiFUIOnJpw/DzoKOhkADgY0Hs03yRyyXV92Qpp+CdWLslNPrtvGjiOpxPXRS9+czust89SYwT6WpLn8UZBRwTAeZCCjgiAEywFLRKAMzcFHREAWQIKOs1D8hqgZQWQraI5CLw2eAezzJSvvBqbzzReo0rVqkG8T/fRw+9TgoqXSlefGcXny1OAzkwMRalkNSmZKw2XbrJh2w1WpmmrY5oXn4+Z2Z9eD1PyyS5/qnyz9eUs96tVZTNUpj/HKVUVTTnL+aEZ75NWNvOZW8DZdNRWejERRgOzk6KH4MVeAjN12e/2Ehi/0sNhTpEZCff0aYDeue6nHtKE5DQmcZanWRwZl/4Bs+AChA=="
 
 local load = false
 
@@ -40,7 +40,8 @@ local pumps = {
 player.set_quick_bar_slot(1, 1, pumps[1])
 player.set_quick_bar_slot(1, 2, pumps[2])
 local pump = nil
-
+local engine = nil
+---@cast engine -?
 
 local story_table =
 {
@@ -61,13 +62,25 @@ local story_table =
 
         player.insert({name=pump, count=6})
 
+        local engines = surface.find_entities_filtered{name = "cargo_ship_engine"}
+        if #engines == 0 then
+          error("Cargo ship engine not found")
+        end
+        engine = engines[1]
+
         if load then
           local tank = surface.find_entities_filtered{name = "storage-tank"}[1]
           tank.set_fluid(1, {name = "crude-oil", amount = 25000})
         else
-          local tanker = surface.find_entities_filtered{name = "oil_tanker"}[1]
+          local tanker = engine.train.fluid_wagons[1]
           tanker.set_fluid(1, {name = "crude-oil", amount = 25000})
         end
+      end
+    },
+    {
+      condition = story_elapsed_check(0),
+      action = function()
+        engine.train.speed = 0.05
       end
     },
     {
@@ -164,13 +177,8 @@ local story_table =
       condition = story_elapsed_check(3),
       action = function()
         player.character.clear_items_inside()
-        local engines = surface.find_entities_filtered{area = {{-15, -10}, {15, 10}}, name = "cargo_ship_engine"}
-        if #engines == 0 then
-          error("Cargo ship engine not found")
-        end
-        local engine = engines[1]
 
-        ---@cast engine -?
+        -- Delete the boat before the waterway
         for _, entity in pairs(engine.train.carriages) do
           entity.destroy()
         end
